87b326211a7821a52bbb9244db1257376dd8be26
Juan Ramos <jars4u2@gmail.com>
2023-08-17 21:24:42 +0000

    register, login and registered done

--- src/api/routes.py
+++ src/api/routes.py
@@ -9,6 +9,7 @@ from api.utils import generate_sitemap, APIException
 from werkzeug.security import generate_password_hash, check_password_hash
 from base64 import b64encode
 import os
+from flask_jwt_extended import create_access_token
 
 api = Blueprint('api', __name__)
 
@@ -19,6 +20,7 @@ def check_password(hash_password, password, salt):
     return check_password_hash(hash_password, f"{password}{salt}")
 
 
+# REGISTRAR USUARIO:
 @api.route('/register', methods=['POST'])
 def register_user():
     if request.method == 'POST':
@@ -43,7 +45,7 @@ def register_user():
                 return jsonify({"message" : "f'error: {err.args}"}), 500
 
 
-
+# INGRESO DE USUARIO:
 @api.route('/login', methods=['POST'])
 def handle_login():
     if request.method == 'POST':
@@ -61,9 +63,17 @@ def handle_login():
             
             else:
                 if check_password(user.password, password, user.salt):
-                    return jsonify({"token" : "locuratotal"}), 200
+                    token = create_access_token(identity=user.id)
+                    return jsonify({"token" : token }), 200
 
                 else:
                     return jsonify({"message" : "Bad Credentials"}), 400
 
-        
+
+# OBTENER USUARIOS:
+@api.route('/register', methods=['GET'])
+def get_registered():
+    if request.method == 'GET':
+        registered = User.query.all()
+
+        return jsonify(list(map(lambda item: item.serialize(), registered)))
